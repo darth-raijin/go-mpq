bedeb99f4e8fd0baab0e25ad7a91c3ad18a20cda
Mohamed <58916277+darth-raijin@users.noreply.github.com>
2024-04-13 16:34:58 +0200

    Create queues.go

---
+++ internal/messagers/queues.go
@@ -0,0 +1,10 @@
+package messagers
+
+/*
+Defining queue names as constants which we can refer to when needed. This way we avoid possible issues with typos in the queue name.
+Furthermore, we get a centralized way of defining our queues. Clean code 👍👍👍
+*/
+const (
+	EventsQueue = "events"
+  LogsQueue = "logs
+)
